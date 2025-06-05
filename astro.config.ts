88bc80c80ee4f1c23776d724c713e57ed9bf0e50
Fernando Ayats <ayatsfer@gmail.com>
2025-06-05 11:45:44 +0200

    Add tailwind

--- astro.config.ts
+++ astro.config.ts
@@ -1,3 +1,11 @@
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({});
+
+export default defineConfig({
+    vite: {
+        plugins: [
+            tailwindcss()
+        ]
+    }
+});
